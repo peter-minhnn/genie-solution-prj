70efb0b00ce995fce5e43ce99e71c9ab552fd112
Minh Nguyen <ldcc.minhnguyen@lotte.vn>
2024-08-25 21:27:05 +0700

    update: config theme

--- app/providers.tsx
+++ app/providers.tsx
@@ -12,7 +12,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
     }, []);
 
     return (
-        <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+        <ThemeProvider attribute="class" enableSystem={false} defaultTheme="light">
             {children}
         </ThemeProvider>
     );
