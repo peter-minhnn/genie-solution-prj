1bf25b398672dce0d8e0fbbb050171d350e956ca
Minh Nguyen <ldcc.minhnguyen@lotte.vn>
2024-09-26 00:34:41 +0700

    fix: build

--- app/api/contact/post/route.ts
+++ app/api/contact/post/route.ts
@@ -18,6 +18,7 @@ export async function POST(req: Request) {
         if (idx !== (-1)) {
             data[idx] = {
                 ...data[idx],
+                id: reqBody.id,
                 name: reqBody.name,
                 message: reqBody.message,
             };
